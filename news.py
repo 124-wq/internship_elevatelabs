8a16ae9faee6b50286521f38445e49334734d27a
Lovely Bisht <bishtlovely28@gmail.com>
2026-02-16 19:27:53 +0530

    adding text file to store headlines

--- news.py
+++ news.py
@@ -9,5 +9,7 @@ html_content = response.text
 soup = BeautifulSoup(html_content,'html.parser')
 news_headlines = soup.find_all(['h1','h2','h3'])
 
-for headline in news_headlines:
-    print(headline.text.strip())
+with open("news_headlines.txt", "w") as f:
+    for headline in news_headlines:
+        f.write(headline.text.strip() + "\n")
+        print(headline.text.strip())
